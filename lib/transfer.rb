1cd68af6c26a23b1c495e979babfb0ca26f5f8c0
Mari <demarius.edwards@gmail.com>
2020-01-09 15:20:18 -0500

    pseudo code

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
+require_relative 'bank_account.rb'
+
 class Transfer
-  # your code here
+  #def initialize - sender, receiver, xfer amount, status = "pending" 
+  #end
+  #valid? return true if account is valid , calls on sender/receiver's valid methods
+  #execute_transaction - create a successful transaction status that makes status complete
+    #a transaction greater than one == false
+  #reverse_transfer - it undoes a transaction between two accounts
+    #it depends on executed_transfers
 end
